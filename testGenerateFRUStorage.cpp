c547a3c8464d6d873075fbeb5a785b3bd50a5da2
Martin Konrad <konrad@frib.msu.edu>
2013-11-14 10:39:32 -0500

    Fix a typo in the --run_test parameter. Clean up main test file.

--- testGenerateFRUStorage.cpp
+++ testGenerateFRUStorage.cpp
@@ -1,9 +1,3 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE generateFRUStorage
 #include <boost/test/unit_test.hpp>
-using namespace boost::unit_test;
-
-// #include "checksumTests.cpp"
-// #include "varLengthLangCodeFieldTests.cpp"
-// #include "commonHeaderTests.cpp"
-// #include "boardInfoAreaTests.cpp"
