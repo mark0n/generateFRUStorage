49934617e5730851619b8ed2bc366f2f63d50f50
Martin Konrad <konrad@frib.msu.edu>
2014-03-19 16:44:10 -0400

    Add unit tests for amcLinkDescriptor.

---
+++ amcLinkDescriptorTests.cpp
@@ -0,0 +1,45 @@
+#include "amcLinkDescriptor.hpp"
+#include "testUtils.hpp"
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE( amcLinkDescriptorTests )
+
+BOOST_AUTO_TEST_CASE( constructorEmpty )
+{
+  struct amcLinkDesignator lnkDesignator = {0x5a, {false, true, false, true}};
+  amcLinkDescriptor ald(lnkDesignator, AMC1PCIe, 2, 0xac, 1);
+}
+
+BOOST_AUTO_TEST_CASE( constructorEmptyGetBinaryData )
+{
+  struct amcLinkDesignator lnkDesignator = {0x5a, {false, true, false, true}};
+  amcLinkDescriptor ald(lnkDesignator, AMC1PCIe, 2, 0xac, 1);
+  std::vector<uint8_t> manResult = { 0x5a, 0x2a, 0x20, 0xac, 0xfd };
+  std::vector<uint8_t> autoResult = ald.getBinaryData();
+  BOOST_CHECK_EQUAL_COLLECTIONS( autoResult.cbegin(), autoResult.cend(), manResult.cbegin(), manResult.cend() );
+  if( autoResult != manResult )
+  {
+    std::cout << "should be: ";
+    std::copy(manResult.cbegin(), manResult.cend(), std::ostream_iterator<int>(std::cout << std::hex, " "));
+    std::cout << std::endl;
+    std::cout << "is:        ";
+    std::copy(autoResult.cbegin(), autoResult.cend(), std::ostream_iterator<int>(std::cout << std::hex, " "));
+    std::cout << std::endl;
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binarySize )
+{
+  struct amcLinkDesignator lnkDesignator = {0x5a, {false, true, false, true}};
+  amcLinkDescriptor ald(lnkDesignator, AMC1PCIe, 2, 0xac, 1);
+  BOOST_CHECK_EQUAL( ald.getBinaryData().size(), 5 );
+}
+
+BOOST_AUTO_TEST_CASE( size )
+{
+  struct amcLinkDesignator lnkDesignator = {0x5a, {false, true, false, true}};
+  amcLinkDescriptor ald(lnkDesignator, AMC1PCIe, 2, 0xac, 1);
+  BOOST_CHECK_EQUAL( ald.size(), 5 );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
