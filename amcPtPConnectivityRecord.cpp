9bf23f557ab927b755fabeb1080c518c9f1fb30b
Martin Konrad <konrad@frib.msu.edu>
2014-03-19 15:01:17 -0400

    Refactor amcPtPConnectivityRecord and fix failed test.
    Fix failed test "size" and remove nasty double memory allocation.

--- amcPtPConnectivityRecord.cpp
+++ amcPtPConnectivityRecord.cpp
@@ -9,10 +9,8 @@
 #define AMC_PTP_CONNECTIVITY_RECORD_TYPE 0x80
 
 amcPtPConnectivityRecord::amcPtPConnectivityRecord(std::list<amcChannelDescriptor> chDescrs, std::list<amcLinkDescriptor> lnkDescrs) :
-  multiRecord::multiRecord(RECORD_TYPE_OEM, std::vector<uint8_t>()),
-  ptPConnRecHeaderRawData(sizeof(*ptPConnRecHeader))
+  multiRecord::multiRecord(RECORD_TYPE_OEM, std::vector<uint8_t>())
 {
-  ptPConnRecHeader = (struct amcPtPConnectivityRecordHeader *)ptPConnRecHeaderRawData.data();
   if(chDescrs.size() > 255) {
     throw std::length_error("list chDescrs exceeds maximum allowed length of 255 entries");
   }
@@ -20,18 +18,22 @@ amcPtPConnectivityRecord::amcPtPConnectivityRecord(std::list<amcChannelDescripto
     throw std::length_error("list lnkDescrs exceeds maximum allowed length of 255 entries");
   }
 
-  ptPConnRecHeader->manufacturerId[2] = PICMG_MANUFACTURER_ID_MSB;
-  ptPConnRecHeader->manufacturerId[1] = PICMG_MANUFACTURER_ID_MID;
-  ptPConnRecHeader->manufacturerId[0] = PICMG_MANUFACTURER_ID_LSB;
-  ptPConnRecHeader->picmgRecordId = AMC_PTP_CONNECTIVITY_RECORD_PICMG_RECORD_ID;
-  ptPConnRecHeader->recordFormatVersion = AMC_PTP_CONNECTIVITY_RECORD_FORMAT_VERSION;
-  ptPConnRecHeader->oemGuidCount = 0;
   recordType = AMC_PTP_CONNECTIVITY_RECORD_TYPE;
+  ptPConnRecHeader.manufacturerId[2] = PICMG_MANUFACTURER_ID_MSB;
+  ptPConnRecHeader.manufacturerId[1] = PICMG_MANUFACTURER_ID_MID;
+  ptPConnRecHeader.manufacturerId[0] = PICMG_MANUFACTURER_ID_LSB;
+  ptPConnRecHeader.picmgRecordId = AMC_PTP_CONNECTIVITY_RECORD_PICMG_RECORD_ID;
+  ptPConnRecHeader.recordFormatVersion = AMC_PTP_CONNECTIVITY_RECORD_FORMAT_VERSION;
+  ptPConnRecHeader.oemGuidCount = 0;
 
   amcChannelDescriptors = chDescrs;
   amcLinkDescriptors = lnkDescrs;
-  
-  int payloadSize = ptPConnRecHeaderRawData.size();
+}
+
+void amcPtPConnectivityRecord::updateRecordLength()
+{
+  int payloadSize = sizeof(ptPConnRecHeader);
+  payloadSize += 2; // recordType and amcChannelDescriptorCount
   for(std::list<amcChannelDescriptor>::const_iterator li = amcChannelDescriptors.cbegin(); li != amcChannelDescriptors.cend(); li++) {
     payloadSize += li->size();
   }
@@ -44,19 +46,19 @@ amcPtPConnectivityRecord::amcPtPConnectivityRecord(std::list<amcChannelDescripto
   header.recordLength = payloadSize;
 }
 
-void amcPtPConnectivityRecord::addAMCChannelDescriptor(int lanePortNo[4]) {
+void amcPtPConnectivityRecord::addAMCChannelDescriptor( int lanePortNo[4] ) {
   for(int lane = 0; lane < 4; lane++) {
-    if(lanePortNo[lane] < 0 || lanePortNo[lane] > 31) {
+    if( lanePortNo[lane] < 0 || lanePortNo[lane] > 31 ) {
       throw std::out_of_range("lane number out of range");
     }
   }
 }
 
 std::vector<uint8_t> amcPtPConnectivityRecord::getBinaryData() {
-  payload = ptPConnRecHeaderRawData;
-  
-  payload.push_back(recordType);
-  payload.push_back(amcChannelDescriptors.size());
+  updateRecordLength();
+  payload = std::vector<uint8_t>( (uint8_t *)&ptPConnRecHeader, (uint8_t *)(&ptPConnRecHeader + 1) );
+  payload.push_back( recordType );
+  payload.push_back( amcChannelDescriptors.size() );
   
   for(std::list<amcChannelDescriptor>::const_iterator li = amcChannelDescriptors.begin(); li != amcChannelDescriptors.end(); li++) {
     std::vector<uint8_t> chDescriptor = li->getBinaryData();
@@ -68,12 +70,11 @@ std::vector<uint8_t> amcPtPConnectivityRecord::getBinaryData() {
     std::copy(lnkDescriptor.begin(), lnkDescriptor.end(), std::back_inserter(payload));
   }
   
-  header.recordLength = payload.size();
-
   return multiRecord::getBinaryData();
 }
 
 void amcPtPConnectivityRecord::printData() {
+  updateRecordLength();
   updateRecordChecksum();
   updateHeaderChecksum();
   std::cout << "Record Type ID: 0x" << std::hex << (unsigned int)header.recordTypeId << std::endl;
@@ -82,11 +83,11 @@ void amcPtPConnectivityRecord::printData() {
   std::cout << "Record length: " << std::dec << (unsigned int)header.recordLength << std::endl;
   std::cout << "Record checksum: 0x" << std::hex << (unsigned int)header.recordChecksum << std::endl;
   std::cout << "Header checksum: 0x" << std::hex << (unsigned int)header.headerChecksum << std::endl;
-  const unsigned int manId = ptPConnRecHeader->manufacturerId[2] << 16 | ptPConnRecHeader->manufacturerId[1] << 8 | ptPConnRecHeader->manufacturerId[0];
+  const unsigned int manId = ptPConnRecHeader.manufacturerId[2] << 16 | ptPConnRecHeader.manufacturerId[1] << 8 | ptPConnRecHeader.manufacturerId[0];
   std::cout << "Manufacturer ID: 0x" << std::hex << manId << std::endl;
-  std::cout << "PICMG Record ID: 0x" << std::hex << (unsigned int)ptPConnRecHeader->picmgRecordId << std::endl;
-  std::cout << "Record Format Version: " << std::dec << (unsigned int)ptPConnRecHeader->recordFormatVersion << std::endl;
-  std::cout << "OEM GUID Count: " << std::dec << (unsigned int)ptPConnRecHeader->oemGuidCount << std::endl;
+  std::cout << "PICMG Record ID: 0x" << std::hex << (unsigned int)ptPConnRecHeader.picmgRecordId << std::endl;
+  std::cout << "Record Format Version: " << std::dec << (unsigned int)ptPConnRecHeader.recordFormatVersion << std::endl;
+  std::cout << "OEM GUID Count: " << std::dec << (unsigned int)ptPConnRecHeader.oemGuidCount << std::endl;
   std::cout << "AMC Channel Descriptors: { " << std::endl;
   std::list<amcChannelDescriptor>::iterator cdli;
   int i;
@@ -103,4 +104,10 @@ void amcPtPConnectivityRecord::printData() {
     ldli->printData();
   }
   std::cout << "}" << std::endl;
+};
+
+int amcPtPConnectivityRecord::size()
+{
+  updateRecordLength();
+  return sizeof(header) + header.recordLength;
 };
--- amcPtPConnectivityRecord.hpp
+++ amcPtPConnectivityRecord.hpp
@@ -14,31 +14,17 @@ struct amcPtPConnectivityRecordHeader {
 };
 
 class amcPtPConnectivityRecord : public multiRecord {
-  std::vector<uint8_t> ptPConnRecHeaderRawData;
-  amcPtPConnectivityRecordHeader *ptPConnRecHeader;
+  amcPtPConnectivityRecordHeader ptPConnRecHeader;
   uint8_t recordType;
   std::list<amcChannelDescriptor> amcChannelDescriptors;
   std::list<amcLinkDescriptor> amcLinkDescriptors;
-//   std::vector<uint8_t> headerRawData;
-//   struct multiRecordHeader *header;
-//   std::vector<uint8_t> payload;
-//   void updateRecordChecksum();
-//   void updateHeaderChecksum();
 public :
   amcPtPConnectivityRecord(std::list<amcChannelDescriptor> chDescrs, std::list<amcLinkDescriptor> lnkDescrs);
+  void updateRecordLength();
   void addAMCChannelDescriptor(int lanePortNo[4]);
-//   multiRecord();
-//   addAMCChannelDescriptor(int lanePortNo[4]);
-//   multiRecord(uint8_t typeId, std::vector<uint8_t> payload);
-//   uint8_t getFormatVersion();
-//   uint8_t getEndOfList();
-//   void setEndOfList(bool endOfList);
-//   uint8_t getRecordLength();
-//   uint8_t getChecksum();
-//   uint8_t getHeaderChecksum();
   std::vector<uint8_t> getBinaryData();
   void printData();
-//   int size();
+  int size();
 };
 
 #endif /* AMCPTPCONNECTIVITYRECORD_HPP */
