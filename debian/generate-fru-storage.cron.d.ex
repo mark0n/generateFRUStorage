15fb46bddf94e2a4cdfb816d4dce0bc6a490b042
Martin Konrad <konrad@frib.msu.edu>
2015-03-19 14:10:52 +0000

    Customize debian directory

--- debian/generate-fru-storage.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the generate-fru-storage package
-#
-0 4	* * *	root	[ -x /usr/bin/generate-fru-storage_maintenance ] && /usr/bin/generate-fru-storage_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(generate-fru-storage):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="generate-fru-storage" command="/usr/bin/generate-fru-storage"
